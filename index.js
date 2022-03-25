a8ce60b52894eedb62606a42f027e8d1cd556f07
Johan Nilsson <yoohahn@gmail.com>
2022-03-25 23:52:07 +0100

    use git to get message

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #!/bin/node
 /// <reference types="node" />
-const git = require("simple-git").default;
 const { spawnSync } = require("child_process");
 const { getPackageVersion } = require("./helpers");
 
@@ -16,12 +15,17 @@ const exec = (str) => {
 };
 
 const run = async () => {
+  let message;
   if (!process.env.GITHUB_TOKEN) {
     throw new Error("Requires GITHUB_TOKEN");
   }
 
-  const log = await git().log();
-  const { message } = log.latest;
+  try {
+    message = execSync("git log -n 1 --pretty=format:'%s'").toString();
+  } catch (e) {
+    console.error("Error when fetching latest commit message");
+    throw e;
+  }
 
   if (message.length > 0) {
     const version = getPackageVersion(message);
